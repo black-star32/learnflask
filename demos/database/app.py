02209570aa13b602bc4beec07cf006b1d3e17300
fengbo <fengbo@fengbodeMac.local>
2019-04-10 10:39:03 +0800

    20190410

--- demos/database/app.py
+++ demos/database/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+import os
+from flask import Flask, flash, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine, Column, Integer, String
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost:8889/data'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret string')
 
 db = SQLAlchemy(app)
 # print(db)
@@ -15,4 +20,26 @@ class Note(db.Model):
 
     def __repr__(self):
         return '<Note {}>'.format(self.body)
+
+class NewNoteForm(FlaskForm):
+    body = TextAreaField('Body', validators=[DataRequired])
+    submit = SubmitField('Save')
+
+@app.route('/')
+def index():
+    form = NewNoteForm()
+    notes = Note.query.all()
+    return render_template('index.html', notes=notes, form=form)
+
+@app.route('/new', methods=['GET', 'POST'])
+def new_note():
+    form = NewNoteForm()
+    if form.validate_on_submit():
+        body = form.body.data
+        note = Note(body=body)
+        db.session.add(note)
+        db.session.commit()
+        flash('Your note is saved.')
+        return redirect(url_for('index'))
+    return render_template('new_note.html', form=form)
 
