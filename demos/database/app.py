a35385f60bf88438081605663e9f943eb60b4340
fengbo <fengbo@fengbodeMac.local>
2019-04-15 09:26:36 +0800

    20190415

--- demos/database/app.py
+++ demos/database/app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, flash, redirect, render_template, url_for
+import click
+from flask import Flask, flash, redirect, render_template, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, SubmitField
@@ -14,6 +15,38 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret string')
 db = SQLAlchemy(app)
 # print(db)
 
+# handlers
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, Note=Note, Article=Article, Author=Author, Writer=Writer, Book=Book, City=City,
+                Citizen=Citizen)
+
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    """Initialize the database."""
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
+# Forms
+class NewNoteForm(FlaskForm):
+    body = TextAreaField('Body', validators=[DataRequired()])
+    submit = SubmitField('Save')
+
+
+class EditNoteForm(FlaskForm):
+    body = TextAreaField('Body', validators=[DataRequired()])
+    submit = SubmitField('Update')
+
+
+class DeleteNoteForm(FlaskForm):
+    submit = SubmitField('Delete')
+
+
+# Models
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
@@ -21,16 +54,14 @@ class Note(db.Model):
     def __repr__(self):
         return '<Note {}>'.format(self.body)
 
-class NewNoteForm(FlaskForm):
-    body = TextAreaField('Body', validators=[DataRequired])
-    submit = SubmitField('Save')
-
+# read
 @app.route('/')
 def index():
-    form = NewNoteForm()
+    form = DeleteNoteForm()
     notes = Note.query.all()
     return render_template('index.html', notes=notes, form=form)
 
+# add
 @app.route('/new', methods=['GET', 'POST'])
 def new_note():
     form = NewNoteForm()
@@ -42,4 +73,145 @@ def new_note():
         flash('Your note is saved.')
         return redirect(url_for('index'))
     return render_template('new_note.html', form=form)
+
+# update
+@app.route('/edit/<int:note_id>', methods=['GET', 'POST'])
+def edit_note(note_id):
+    form = EditNoteForm()
+    note = Note.query.get(note_id)
+    if form.validate_on_submit():
+        note.body = form.body.data
+        db.session.commit()
+        flash('Your note is updated.')
+        return redirect(url_for('index'))
+    form.body.data = note.body
+    return render_template('edit_note.html', form=form)
+
+# Delete
+@app.route('/delete/<int:note_id>', methods=['POST'])
+def delete_note(note_id):
+    form = DeleteNoteForm()
+    if form.validate_on_submit():
+        note = Note.query.get(note_id)
+        db.session.delete(note)
+        db.session.commit()
+        flash('Your note is deleted.')
+    else:
+        abort(400)
+    return redirect(url_for('index'))
+
+# one to many
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
+    phone = db.Column(db.String(20))
+    articles = db.relationship('Article')  # collection
+
+    def __repr__(self):
+        return '<Author {}>'.format()
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), index=True)
+    body = db.Column(db.Text)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+
+    def __repr__(self):
+        return '<Article {}>'.format(self.title)
+
+# one to many + bidirectional relationship
+class Writer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    books = db.relationship('Book', back_populates='writer')
+
+    def __repr__(self):
+        return '<Writer %r>' % self.name
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), index=True)
+    writer_id = db.Column(db.Integer, db.ForeignKey('writer.id'))
+    writer = db.relationship('Writer', back_populates='books')
+
+    def __repr__(self):
+        return '<Book %r>' % self.name
+
+# one to many + bidirectional relationship + use backref to declare bidirectional relationship
+class Singer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), unique=True)
+    songs = db.relationship('Song', backref='singer')
+
+    def __repr__(self):
+        return '<Singer %r>' % self.name
+
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), index=True)
+    singer_id = db.Column(db.Integer, db.ForeignKey('singer.id'))
+
+    def __repr__(self):
+        return '<Song %r>' % self.name
+
+# many to one
+class Citizen(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), unique=True)
+    city_id = db.Column(db.Integer, db.ForeignKey('city.id'))
+    city = db.relationship('City')  # scalar
+
+    def __repr__(self):
+        return '<Citizen %r>' % self.name
+
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+
+    def __repr__(self):
+        return '<City %r>' % self.name
+
+# one to one
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    capital = db.relationship('Capital', uselist=False)
+
+    def __repr__(self):
+        return '<Country {}>'.format(self.name)
+
+class Capital(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'))
+    country = db.relationship('Country')
+
+    def __repr__(self):
+        return '<Capital {}>'.format(self.name)
+
+# many to many with association table
+association_table = db.Table('association', db.Column('student_id', db.Integer,
+    db.ForeignKey('student.id')), db.Column('teacher_id', db.Integer, db.ForeignKey('teacher.id')))
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), unique=True)
+    grade = db.Column(db.String(20))
+    teachers = db.relationship('Teacher', secondary=association_table, back_populates='students')
+
+    def __repr__(self):
+        return '<Student {}>'.format(self.name)
+
+class Teacher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), unique=True)
+    office = db.Column(db.String(20))
+    students = db.relationship('Student', secondary=association_table, back_populates='teachers')
+
+    def __repr__(self):
+        return '<Teacher {}>'.format(self.name)
 
